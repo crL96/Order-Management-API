e51147b395834074279f9ff05ce2f631bd9fcf1f
crL96 <190506813+crL96@users.noreply.github.com>
2025-08-07 10:39:22 +0200

    Add route and controller for getting a product by id

--- src/controllers/inventoryController.js
+++ src/controllers/inventoryController.js
@@ -10,6 +10,26 @@ async function getAllProducts(req, res) {
     }
 }
 
+async function getProduct(req, res) {
+    try {
+        const product = await prisma.product.findUnique({
+            where: {
+                id: req.params.productId,
+            }
+        })
+        if (product === null) {
+            res.status(404).send("No product found");
+            return;
+        }
+        product.stockValue = product.price * product.stock;
+        res.json(product);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Internal server error");
+    }
+}
+
 module.exports = {
     getAllProducts,
+    getProduct,
 }
--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("../controllers/inventoryController");
 
 router.get("/all", controller.getAllProducts);
+router.get("/product/:productId", controller.getProduct);
 
 module.exports = router;

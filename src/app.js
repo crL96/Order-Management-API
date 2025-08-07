affc81698a5487a103b68ee68c8285fb9119d7c5
crL96 <190506813+crL96@users.noreply.github.com>
2025-08-07 09:26:18 +0200

    Add and setup basic initial express server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+//App Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Server running");
+});
+
+const port = process.env.PORT | 3000;
+app.listen(port, () => {
+    console.log("Server running on port: " + port);
+})
